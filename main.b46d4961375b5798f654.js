e38ef3cf152b82c288b1d1fb47d4056a7555e5b1
Andrey Klemenko <web.klemenko@gmail.com>
2021-02-28 23:04:17 +0200

    Updates

--- main.b46d4961375b5798f654.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{L1EO:function(t,e,n){},QfWi:function(t,e,n){"use strict";n.r(e);n("L1EO");var o=document.querySelector('[data-action="start"]');document.querySelector('[data-action="stop"]'),document.querySelector("body");o.addEventListener("click",(function(){}))}},[["QfWi",1]]]);
-//# sourceMappingURL=main.b46d4961375b5798f654.js.map
